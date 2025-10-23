aa42fa7904be0097cc8163d42fcf88d79d2cd915
harumaki1220 <haruma181220@gmail.com>
2025-10-24 02:29:54 +0900

    feat: 変換ロジックの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 
+const convertToHohokekyo = (originalText: string): string => {
+  if (!originalText) {
+    return '';
+  }
+
+  const chars = originalText.split('');
+
+  const encodedChars = chars.map((char) => {
+    const charCode = char.charCodeAt(0);
+
+    const base3String = charCode.toString(3);
+
+    const hohokekyoString = base3String
+      .replace(/0/g, 'ホ')
+      .replace(/1/g, 'ケ')
+      .replace(/2/g, 'キョ');
+
+    return hohokekyoString;
+  });
+
+  return encodedChars.join('ー');
+};
+
 function App() {
   const [text, setText] = useState<string>('');
+  const convertedText = convertToHohokekyo(text);
 
   return (
     <div className={styles.container}>
@@ -17,7 +41,7 @@ function App() {
         onChange={(e) => setText(e.target.value)}
       />
 
-      <textarea rows={10} className={styles.textareaOutput} value={text} readOnly />
+      <textarea rows={10} className={styles.textareaOutput} value={convertedText} readOnly />
     </div>
   );
 }
